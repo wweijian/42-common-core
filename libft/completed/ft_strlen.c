fc35592a09a14de50fceb68e07c45d3e8b056e17
wweijian <hoe.weijian@gmail.com>
2025-05-07 10:33:37 +0800

    standard push

---
+++ libft/completed/ft_strlen.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wjhoe <wjhoe@student.42singapore.sg>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/06 16:35:04 by wjhoe             #+#    #+#             */
+/*   Updated: 2025/05/07 00:07:10 by wjhoe            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlen(const char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+/* #include <stdio.h>
+#include <string.h>
+
+int	main (void)
+{
+	printf("ft_strlen-strlen -> %lu\n", ft_strlen("hello") - strlen("hello"));
+	printf("ft_strlen-strlen -> %lu\n", ft_strlen("") - strlen(""));
+	printf("ft_strlen-strlen -> %lu\n", ft_strlen("helasd   ") - 
+		strlen("helasd   "));
+
+}
+ */
