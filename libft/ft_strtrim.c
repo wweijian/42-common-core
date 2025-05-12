c0d04696497a1e32058ca4aa90e2f7324bdc901e
wweijian <hoe.weijian@gmail.com>
2025-05-12 18:20:10 +0800

    ft_strtrim.c

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: wjhoe <wjhoe@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/09 12:59:08 by wjhoe             #+#    #+#             */
-/*   Updated: 2025/05/09 16:27:45 by wjhoe            ###   ########.fr       */
+/*   Updated: 2025/05/12 18:19:53 by wjhoe            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,22 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	char	*s1buf;
-	char	*setbuf;
+	size_t	start;
+	size_t	end;
+	char	*res;
 
-	if (!s1 || !set)
+	if (!s1)
 		return (NULL);
-	s1buf = ft_strdup(s1);
-	if (!s1buf)
-		return (NULL);
-	setbuf = ft_strnstr(s1buf, set, ft_strlen(s1buf));
-	if (!setbuf)
-		return (NULL);
-	setbuf = ft_memmove(setbuf, setbuf + ft_strlen(set), ft_strlen(s1buf));
-	return (s1buf);
+	if (!set)
+		return ((char *)s1);
+	start = 0;
+	while (s1[start] && ft_strchr(set, s1[start]))
+		start++;
+	end = ft_strlen(s1);
+	while (end > start && ft_strchr(set, s1[end - 1]))
+		end--;
+	res = ft_substr(s1, start, end - start);
+	return (res);
 }
 
 /* #include <string.h>
@@ -34,33 +37,33 @@ char	*ft_strtrim(char const *s1, char const *set)
 
 int main ()
 {
-	char *s1 = strdup("qwertyuiopasdfghjklzxcvbnm");
-	char *exist = strdup("asdfghjkl");
-	char *last = strdup("zxcvbnm");
-	char *first = strdup("qwerty");
-	char *noexist = strdup("aa");
+	char *s1 = strdup("qwertyuiopasdfghjklzxcvbnmertyl");
+	// char *exist = strdup("asdfghjkl");
+	// char *last = strdup("zxcvbnm");
+	// char *first = strdup("qwerty");
+	// char *noexist = strdup("aa");
 
-	char *s1trim = ft_strtrim(s1,s1);
-	char *existtrim = ft_strtrim(s1,exist);
-	char *lasttrim = ft_strtrim(s1,last);
-	char *firsttrim = ft_strtrim(s1,first);
-	char *noexisttrim = ft_strtrim(s1,noexist);
+	char *s1trim = ft_strtrim(s1,"qwerty");
+	// char *existtrim = ft_strtrim(s1,exist);
+	// char *lasttrim = ft_strtrim(s1,last);
+	// char *firsttrim = ft_strtrim(s1,first);
+	// char *noexisttrim = ft_strtrim(s1,noexist);
 
 	printf("s1 : s1---------    %s\n", s1trim);
-	printf("s1 : exist------    %s\n", existtrim);
-	printf("s1 : last-------    %s\n", lasttrim);
-	printf("s1 : first------    %s\n", firsttrim);
-	printf("s1 : noexist----    %s\n", noexisttrim);
+	// printf("s1 : exist------    %s\n", existtrim);
+	// printf("s1 : last-------    %s\n", lasttrim);
+	// printf("s1 : first------    %s\n", firsttrim);
+	// printf("s1 : noexist----    %s\n", noexisttrim);
 
 	free(s1);
-	free(exist);
-	free(last);
-	free(first);
-	free(noexist);
-	free(s1trim);
-	free(existtrim);
-	free(lasttrim);
-	free(firsttrim);
-	free(noexisttrim);
+	// free(exist);
+	// free(last);
+	// free(first);
+	// free(noexist);
+	// free(s1trim);
+	// free(existtrim);
+	// free(lasttrim);
+	// free(firsttrim);
+	// free(noexisttrim);
 
 } */
